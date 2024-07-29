a109c2d7d60ef0df9cbf56b8d3b231d56f674891
chandusiriyala <chandusiriyala116@gmail.com>
2024-07-30 03:34:29 +0530

    Add files via upload

---
+++ Frontend/src/components/loginValidation.js
@@ -0,0 +1,29 @@
+function validation(values) {
+    let error = {}
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
+    if(values.email === ''){
+        error.email = "Email should bot be empty"
+    }
+    else if(!emailPattern.test(values.email)){
+        error.email = "Email Didn't match"
+    }
+    else{
+        error.email = ""
+    }
+
+    if(values.password === ''){
+        error.password = "Password should bot be empty"
+    }
+    else if(!passwordPattern.test(values.password)){
+        error.password = "Password Didn't match"
+    }
+    else{
+        error.password = ""
+    }
+
+    return error;
+
+}
+export default validation
